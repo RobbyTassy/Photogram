6573b3407db4c82394f79f2727f2d1af5645a660
Robert Tassy <Rtass001@fiu.edu>
2016-05-06 16:13:58 -0400

    set_post in post controller to dry up id paramater calls

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destory]
+
   def index
     @post = Post.all
   end
@@ -13,21 +15,17 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to(post_path(@post))
   end
 
   def destroy
-    @post = Post.find(param[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -35,6 +33,10 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(param[:id])
   end
 
 end
